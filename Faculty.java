716ca2e9a039d695325af78985cbd882f0529d8d
BravoAna19 <bravoana49@yahoo.com>
2020-04-10 01:15:49 +0200

    Added Faculty.java to the new branch

---
+++ Faculty.java
@@ -0,0 +1,18 @@
+class Faculty{
+
+	Student *list;
+	String imeFakultet;
+	
+	void dodadiStudent(Student s){
+			
+	}
+	
+	void izbrishiStudent(){
+
+	}
+	
+	float prosecenBrojKrediti()
+		return prosek;
+	}
+
+}
