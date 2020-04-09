f1b1fc8f8d416bd4830c6249fe623354fd945b20
BravoAna19 <bravoana49@yahoo.com>
2020-04-10 00:57:51 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -2,11 +2,46 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-	int * grades;
+	int numberGrades;
+	int  *grades;
 
 	//TODO constructor
+	Student(String index, String firstName, String lastName, int numberGrades, int *grades){
+		this.index=index;
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.numberGrades=numberGrades;
+		for (int i=0; i<numberGrades; i++){
+			this.grades[i]=grades[i];
+		}
+	}
 
 	//TODO seters & getters
+	String getIndex(){
+		return index;
+	}
+
+	void setIndex(index){
+		this.index=index;
+	}
+
+	String getFirstName(){
+		return firstName;
+	}
+        void setFirstName(firstName){
+                this.firstName=firstName;
+        }
+
+	String getLastName(){
+		return lastName;
+	}
+        void setLastName(lastName){
+                this.lastName=firstName;
+        }
+
+	void setNumberGrades(numberGrades){
+		this.numberGrades=numberGrades;
+	}
 
 	public double getAverage() {
 		//TODO
